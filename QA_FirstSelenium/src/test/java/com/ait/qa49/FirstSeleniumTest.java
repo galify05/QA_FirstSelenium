44e889d9ee8dff291379a653899245fa300f90f3
unknown <galify05@gmail.com>
2024-11-18 15:06:33 +0100

    homework

--- QA_FirstSelenium/src/test/java/com/ait/qa49/FirstSeleniumTest.java
+++ QA_FirstSelenium/src/test/java/com/ait/qa49/FirstSeleniumTest.java
@@ -42,3 +42,6 @@ public class FirstSeleniumTest {
         driver.quit();
     }
 }
+
+
+
---
+++ QA_FirstSelenium/src/test/java/com/ait/qa49/Homework.java
@@ -0,0 +1,34 @@
+package com.ait.qa49;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+
+    driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get("https://demowebshop.tricentis.com/");
+
+    }
+
+    @Test
+public void openWebsite(){
+        System.out.println("Страница открыта");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
