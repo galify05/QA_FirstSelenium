244b80381037b016a7b081e8ac0bb67040bfee21
unknown <galify05@gmail.com>
2024-11-18 16:54:26 +0100

    second homework

--- QA_FirstSelenium/src/test/java/com/ait/qa49/FindElementTests.java
+++ QA_FirstSelenium/src/test/java/com/ait/qa49/FindElementTests.java
@@ -18,7 +18,7 @@ public class FindElementTests {
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
-        driver.get("https://ilcarro.web.app/search");
+        driver.get("https://ilcarro.web.app");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
@@ -30,58 +30,50 @@ public class FindElementTests {
 
     @Test
     public void findElementByTagName() {
+        //find element by tag name
         WebElement element = driver.findElement(By.tagName("h1"));
         System.out.println(element.getText());
 
         WebElement element1 = driver.findElement(By.tagName("a"));
-        System.out.println(element1.getText());
+        System.out.println(element1.getText());// no text
 
-//находим список элементов
+        //find list of elements by tag name
         List<WebElement> elements = driver.findElements(By.tagName("a"));
         System.out.println(elements.size());
     }
 
     @Test
     public void findElementBySimpleLocators() {
-        //by id
+        // by id
         driver.findElement(By.id("city"));
-        //by class name
+        //by className
         driver.findElement(By.className("header"));
-
-        //by link Text
-        WebElement carWork = driver.findElement(By.linkText("Let the car work"));
-        System.out.println(carWork);
-
-        WebElement car = driver.findElement(By.partialLinkText("car"));
-        System.out.println(car);
+        //by linkText
+        driver.findElement(By.linkText("Let the car work"));
+        //by partialLinkText
+        driver.findElement(By.partialLinkText("car"));
     }
 
     @Test
     public void findElementByCssSelectorTest() {
-//        tag Name == css
-//        driver.findElement(By.tagName("h1"));
+        //tagName == css
+        //  driver.findElement(By.tagName("h1"));
         driver.findElement(By.cssSelector("h1"));
-//        id=> css(#)
-//    driver.findElement(By.id("city"));
+        //id -> css(#)
+        //driver.findElement(By.id("city"));
         driver.findElement(By.cssSelector("#city"));
-
-//        classname css
-//        driver.findElement(By.className("header"));
+        //className -> css(.)
+        // driver.findElement(By.className("header"));
         driver.findElement(By.cssSelector(".header"));
-
-
-        driver.findElement(By.cssSelector("[href=\"/login?url=%2Fsearch\"]"));
-        //contains *
-        driver.findElement(By.cssSelector("[href*=\"login\"]"));
-//        driver.findElement(By.cssSelector("[href*='login']"));
-
-//start ^ начало
-        driver.findElement(By.cssSelector("[href^=\"/log\"]"));
-
-//    end $ конец
-        driver.findElement(By.cssSelector("[href$=\"search\"]"));
-
+        // [attr = 'value']
+        driver.findElement(By.cssSelector("[href='/login?url=%2Fsearch']"));
+        //contains -> *
+        driver.findElement(By.cssSelector("[href*='login']"));
+        //start -> ^
+        driver.findElement(By.cssSelector("[href^='/log']"));
+        //end on-> $
+        driver.findElement(By.cssSelector("[href$='search']"));
     }
 }
 
-//
+
--- QA_FirstSelenium/src/test/java/com/ait/qa49/Homework.java
+++ QA_FirstSelenium/src/test/java/com/ait/qa49/Homework.java
@@ -1,33 +1,94 @@
 package com.ait.qa49;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
 
 public class Homework {
 
     WebDriver driver;
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
 
-    driver = new ChromeDriver();
-    driver.manage().window().maximize();
-    driver.get("https://demowebshop.tricentis.com/");
-
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demowebshop.tricentis.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
     }
 
     @Test
-public void openWebsite(){
+    public void openWebsite() {
         System.out.println("Страница открыта");
     }
 
+    @Test
+    public void findElementByTagName() {
+        // 1
+        List<WebElement> divElements = driver.findElements(By.tagName("div"));
+        System.out.println(divElements.size());
+        //2
+        driver.findElement(By.tagName("a"));
+    }
+
+    @Test
+    public void findElementBySimpleLocatorsTest() {
+        //3
+        driver.findElement(By.id("flyout-cart"));
+        //4
+        driver.findElement(By.className("footer-store-theme"));
+        //5
+        driver.findElement(By.linkText("Log in"));
+        //6
+        List<WebElement> logElements = driver.findElements(By.partialLinkText("us"));
+        System.out.println("\"us\" on page: " + logElements.size());
+        //7
+        System.out.println("\"li\" elements on page: " + driver.findElements(By.tagName("li")).size());
+        //8
+        driver.findElement(By.tagName("head"));
+        //9
+        driver.findElement(By.id("nivo-slider"));
+        //10
+        driver.findElement(By.partialLinkText("My"));
+    }
+
+    @Test
+    public void cssTesting() {
+        //1
+        driver.findElement(By.cssSelector("#nivo-slider"));
+        //2
+        driver.findElement(By.cssSelector("#flyout-cart"));
+        //3
+        driver.findElement(By.cssSelector(".footer-store-theme"));
+        //4
+        List<WebElement> nivoBoxes = driver.findElements(By.cssSelector(".nivo-box")); // не работает на 2 секундах
+        System.out.println("\"nivo-box\" classes on page: " + nivoBoxes.size());
+        //5
+        driver.findElement(By.cssSelector("span"));
+        //6
+        driver.findElement(By.cssSelector("body"));
+        //7
+        System.out.println(driver.findElement(By.cssSelector("[href=\"/cart\"]")).getText());
+        //8
+        System.out.println(driver.findElement(By.cssSelector("[href=\"/login\"]")).getText());
+        //9
+        System.out.println(driver.findElement(By.cssSelector("[href$='returns']")).getText());
+        //10
+        System.out.println(driver.findElement(By.cssSelector("[href^=\"/shipping\"]")).getText());
+//      driver.findElement(By.cssSelector("[href*='shipping']"));
+
+    }
+
+
     @AfterMethod
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 
